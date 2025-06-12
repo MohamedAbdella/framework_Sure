e9b185784b37d06330fc7f620d33fc5f717aa59d
MohamedAbdella <106119860+MohamedAbdella@users.noreply.github.com>
2025-06-12 14:47:38 +0300

    Fix IOSSimulator inheritance

--- src/main/java/com/sure/utilities/IOSSimulator.java
+++ src/main/java/com/sure/utilities/IOSSimulator.java
@@ -5,18 +5,18 @@ import java.util.concurrent.TimeUnit;
 
 import static com.sure.utilities.FilesDirectories.USER_DIR;
 
-public class IOSSimulator extends AndroidEmulator {
+public class IOSSimulator {
 
     public static void startSimulator() throws IOException, InterruptedException {
         String scriptPath = USER_DIR + "/src/main/resources/startIOSSimulator.sh";
-        executeCommand(scriptPath);
+        AndroidEmulator.executeCommand(scriptPath);
         TimeUnit.SECONDS.sleep(9);
     }
 
 
     public static void stopSimulator() throws IOException, InterruptedException {
         String scriptPath = USER_DIR + "/src/main/resources/stopIOSSimulator.sh";
-        executeCommand(scriptPath);
+        AndroidEmulator.executeCommand(scriptPath);
         TimeUnit.SECONDS.sleep(1);
     }
 }
